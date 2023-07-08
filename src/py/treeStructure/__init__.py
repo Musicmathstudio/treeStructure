87e4b4ee5129d80389adfca4d5663c62056cee9b
Musicmath <pi3141592676@yahoo.com.tw>
2023-07-08 16:35:56 +0800

    Change import format

---
+++ src/py/treeStructure/__init__.py
@@ -0,0 +1,5 @@
+from .tsBinaryNode import TSBinaryNode as tsBinaryNode
+from .tsBinarySearchTree import TSBinarySearchTree as tsBinarySearchTree
+from .tsBinaryHeapNode import TSBinaryHeapNode as tsBinaryHeapNode
+from .tsBinaryHeap import TSBinaryHeap as tsBinaryHeap
+from .tsConstants import TSConstants as tsConstants
