36115caed1d3817e88afe9d5136eb5a22f272145
Musicmath <pi3141592676@yahoo.com.tw>
2023-07-12 12:03:01 +0800

    Write description in each node and update readme

--- src/py/treestructure/binaryHeapNode.py
+++ src/py/treestructure/binaryHeapNode.py
@@ -1,3 +1,7 @@
+"""
+Implementation of binary heap node.
+"""
+
 from time import time
 from typing import Union
 from .binaryNode import BinaryNode
@@ -6,6 +10,13 @@ from .binaryNode import BinaryNode
 class BinaryHeapNode(BinaryNode):
 
     def __init__(self, order: Union[float, int] = time(), value=None):
+        """
+        Binary heap node.
+
+        :param order: It's the priority when constructing the tree structure. Default order is current timestamp.
+        :param value: It can be anything you want to store. Default value is None.
+        """
+
         super().__init__(order, value)
         self.index = -1
         self.leftChildNode: Union[BinaryHeapNode, None] = None
